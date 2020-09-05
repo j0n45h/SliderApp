0bacdbd3f020d19c366f728a3b59784fb90fdb09
Jonas <jonashoelzel22@gmail.com>
2020-09-05 19:17:53 +0200

    Add: End-Time is not set Warning

--- lib/timelapse/ramped_tl/ramp_curve_preview.dart
+++ lib/timelapse/ramped_tl/ramp_curve_preview.dart
@@ -1,7 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sliderappflutter/timelapse/ramped_tl/ramped_graph/ramped_graph_screen.dart';
+import 'package:sliderappflutter/timelapse/ramped_tl/state/time_state.dart';
+import 'package:sliderappflutter/utilities/text_style.dart';
 
 class RampCurvePreview extends StatelessWidget {
   @override
@@ -20,7 +23,7 @@ class RampCurvePreview extends StatelessWidget {
           ),
         ),
         InkWell(
-          onTap: () => Navigator.of(context).pushNamed(RampedGraphScreen.routeName),
+          onTap: () => navigateToRampedGraph(context),
           borderRadius: BorderRadius.circular(15),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
@@ -36,5 +39,26 @@ class RampCurvePreview extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  void navigateToRampedGraph(BuildContext context) {
+    final timeState = Provider.of<TimeState>(context, listen: false);
+    if (timeState.endingTime == null) {
+      _showTimeNotSetSnakeBar(context);
+      return;
+    }
+    Navigator.of(context).pushNamed(RampedGraphScreen.routeName);
+  }
+
+  void _showTimeNotSetSnakeBar(BuildContext context) {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      backgroundColor: Colors.redAccent,
+      elevation: 40,
+      behavior: SnackBarBehavior.fixed,
+      content: Text(
+        'End-Time is not set!',
+        style: MyTextStyle.fetStdSize(),
+      ),
+    ));
   }
 }
