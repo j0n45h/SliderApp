b25f7d391f89628e9eb6fbb2256f1d3a78c1e041
Jonas <jonashoelzel22@gmail.com>
2020-09-11 21:18:52 +0200

    Change: Improve Interval Scale

--- lib/timelapse/ramped_tl/ramped_graph/interval_scale.dart
+++ lib/timelapse/ramped_tl/ramped_graph/interval_scale.dart
@@ -8,52 +8,60 @@ import 'package:sliderappflutter/utilities/text_style.dart';
 class IntervalScale extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final intervalRangeState =
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final intervalRangeState =
         Provider.of<IntervalRangeState>(context, listen: false);
-    var scale = NiceScale(intervalRangeState.intervalRange.start,
-        intervalRangeState.intervalRange.end, 5);
+        final scale = NiceScale(intervalRangeState.intervalRange.start,
+            intervalRangeState.intervalRange.end, 5);
 
-    List<Widget> list = [];
-    var height = MediaQuery.of(context).size.height;
+        List<Widget> list = [];
+        final height = constraints.maxHeight;
 
-    for (var i = scale.niceMin; i <= scale.niceMax; i += scale.tickSpacing) {
-      var pos = map(i, scale.niceMin, scale.niceMax, height - 25, 40);
-      list.add(Positioned(
-        top: pos,
-        child: Row(
+        for (var i = scale.niceMin; i <= scale.niceMax; i += scale.tickSpacing) {
+          final pos = map(i, scale.niceMin, scale.niceMax, height - 50, 50);
+          final scaleNumber = i.floor().toString();
+          list.add(Positioned(
+            top: pos - 26/2,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 4,
+                  height: 1,
+                  color: Colors.grey[500],
+                ),
+                SizedBox(width: 3),
+                Container(
+                  height: 26,
+                  alignment: Alignment.center,
+                  child: Text(
+                    scaleNumber,
+                    style: MyTextStyle.normal(fontSize: 12),
+                  ),
+                ),
+              ],
+            ),
+          ));
+        }
+        return Row(
           children: [
             Container(
-              width: 4,
-              height: 1,
+              margin: const EdgeInsets.only(left: 5),
+              width: 2,
+              height: MediaQuery.of(context).size.height,
               color: Colors.grey[500],
             ),
-            SizedBox(width: 3),
-            Text(
-              i.floor().toString(),
-              style: MyTextStyle.normal(fontSize: 12),
+            Container(
+              width: 24,
+              child: Stack(
+                alignment: Alignment.topLeft,
+                children: list,
+              ),
             ),
           ],
-        ),
-      ));
-    }
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(left: 5),
-          width: 2,
-          height: MediaQuery.of(context).size.height,
-          color: Colors.grey[500],
-        ),
-        Container(
-          height: height,
-          width: 24,
-          child: Stack(
-            alignment: Alignment.topRight,
-            children: list,
-          ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
