a5479524e5f9d1e21e300b1a64e9d9a9f7ac4635
Jonas Hölzel <jonas.hoelzel@htw-dresden.de>
2020-12-20 00:16:37 +0100

    Fix pixel overflow error at start, save buttons

--- lib/timelapse/save_start_buttons.dart
+++ lib/timelapse/save_start_buttons.dart
@@ -10,6 +10,8 @@ class SaveAndStartButtons extends StatelessWidget {
 
   const SaveAndStartButtons({this.onPressSave, this.onPressStart, this.saveButton});
 
+  static const double _spaceBetweenButtons = 32;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,61 +21,66 @@ class SaveAndStartButtons extends StatelessWidget {
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [
-              Colors.black.withOpacity(0.05),
-              Colors.black54,
-              Colors.black87
-            ],
+            colors: [Colors.black.withOpacity(0.05), Colors.black54, Colors.black87],
           ),
         ),
         height: 40,
         padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              width: 160,
-              child: RawMaterialButton(
-                child: Text(
-                  'SAVE',
-                  style: MyTextStyle.fetStdSize(
-                    letterSpacing: 6,
-                    newColor: Colors.black,
-                    fontWight: FontWeight.w400,
-                  ),
-                ),
-                fillColor: Colors.white,
-                onPressed: onPressSave,
-                shape: StadiumBorder(),
-                elevation: 12,
-              ),
-            ),
-            const SizedBox(width: 32),
-            Container(
-              width: 160,
-              child: Builder(builder: (context) {
-                if (saveButton == null)
-                  return RawMaterialButton(
-                    child: Text(
-                      'START',
-                      style: MyTextStyle.fetStdSize(
-                        letterSpacing: 6,
-                        newColor: Colors.black,
-                        fontWight: FontWeight.w400,
-                      ),
+        child: LayoutBuilder(builder: (context, constraints) {
+          // calc Button width
+          final int defaultWidth = 160;
+          final double buttonWidth = constraints.maxWidth / 2 - _spaceBetweenButtons / 2 < defaultWidth
+              ? constraints.maxWidth / 2 - _spaceBetweenButtons / 2
+              : defaultWidth;
+
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: buttonWidth,
+                child: RawMaterialButton(
+                  child: Text(
+                    'SAVE',
+                    style: MyTextStyle.fetStdSize(
+                      letterSpacing: 6,
+                      newColor: Colors.black,
+                      fontWight: FontWeight.w400,
                     ),
-                    fillColor: Color(0xff00FF5F),
-                    onPressed: onPressStart,
-                    shape: StadiumBorder(),
-                    elevation: 12,
-                  );
-                else
-                  return saveButton;
-                },
+                  ),
+                  fillColor: Colors.white,
+                  onPressed: onPressSave,
+                  shape: StadiumBorder(),
+                  elevation: 12,
+                ),
               ),
-            ),
-          ],
-        ),
+              const SizedBox(width: _spaceBetweenButtons),
+              Container(
+                width: buttonWidth,
+                child: Builder(
+                  builder: (context) {
+                    if (saveButton == null)
+                      return RawMaterialButton(
+                        child: Text(
+                          'START',
+                          style: MyTextStyle.fetStdSize(
+                            letterSpacing: 6,
+                            newColor: Colors.black,
+                            fontWight: FontWeight.w400,
+                          ),
+                        ),
+                        fillColor: Color(0xff00FF5F),
+                        onPressed: onPressStart,
+                        shape: StadiumBorder(),
+                        elevation: 12,
+                      );
+                    else
+                      return saveButton;
+                  },
+                ),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
