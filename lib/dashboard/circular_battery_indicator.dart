11b2a380be2e861fe03bf00dd37a9b5e5fd695d9
Jonas Hölzel <jonashoelzel22@gmail.com>
2023-02-15 13:51:26 +0100

    Update Flutter + Packages

--- lib/dashboard/circular_battery_indicator.dart
+++ lib/dashboard/circular_battery_indicator.dart
@@ -17,49 +17,47 @@ class CircularBatteryIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.fromLTRB(40, 35, 10, 45),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          boxShadow: [
-            BoxShadow(
-              color: MyColors.green.withAlpha(0),
-              blurRadius: 40,
-              spreadRadius: 0.0,
-              offset: Offset(
-                0.0,
-                1.0,
-              ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: [
+          BoxShadow(
+            color: MyColors.green.withAlpha(0),
+            blurRadius: 40,
+            spreadRadius: 0.0,
+            offset: Offset(
+              0.0,
+              1.0,
+            ),
+          ),
+        ],
+      ),
+      child: CircularPercentIndicator(
+        linearGradient: LinearGradient(
+          colors: [MyColors.green, Color(0x6600FF3C)],
+          end: Alignment(0.8, 0.0),
+          begin: Alignment.topCenter,
+        ),
+        radius: 50.0,
+        lineWidth: 2.0,
+        percent: batteryPercentage / 100,
+        reverse: true,
+        backgroundColor: Colors.transparent,
+        // progressColor: MyColors.green,
+        center: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              batteryPercentage.toString(),
+              style: TextStyle(
+                  fontFamily: 'Roboto', color: Color(0xffffffff), fontSize: 25, fontWeight: FontWeight.w200),
+            ),
+            Text(
+              '%',
+              style: TextStyle(
+                  fontFamily: 'Roboto', color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.w200),
             ),
           ],
-        ),
-        child: CircularPercentIndicator(
-          linearGradient: LinearGradient(
-            colors: [MyColors.green, Color(0x6600FF3C)],
-            end: Alignment(0.8, 0.0),
-            begin: Alignment.topCenter,
-          ),
-          radius: 100.0,
-          lineWidth: 2.0,
-          percent: batteryPercentage / 100,
-          reverse: true,
-          backgroundColor: Colors.transparent,
-          // progressColor: MyColors.green,
-          center: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Text(
-                batteryPercentage.toString(),
-                style: TextStyle(
-                    fontFamily: 'Roboto', color: Color(0xffffffff), fontSize: 25, fontWeight: FontWeight.w200),
-              ),
-              Text(
-                '%',
-                style: TextStyle(
-                    fontFamily: 'Roboto', color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.w200),
-              ),
-            ],
-          ),
         ),
       ),
     );
